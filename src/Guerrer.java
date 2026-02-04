ebc0e504e8ac4cbb64f7304682c82bfe1ab35e68
Marti Quera <marti.quera@gracia.lasalle.cat>
2026-02-04 05:21:41 +0100

    guerrer: funcio rebre dany acabada i millores generals

--- src/Guerrer.java
+++ src/Guerrer.java
@@ -1,20 +1,36 @@
 class Guerrer extends Entitat {
-    private int resistencia;
+    private float resistencia;
 
     public Guerrer(String n, int lvl, int pV, int res) {
         super(n, lvl, pV);
-        this.resistencia = res;
+        setResistencia(res);
     }
 
     // RESISTENCIA
     public int getResistencia() { return resistencia; }
 
-    public void setResistencia( int res ) { this.resistencia = res; }
+    public void setResistencia( int res ) {
+        this.resistencia = res;
+        if (this.resistencia > 5) {
+            this.resistencia = 5;
+        } else if (this.resistencia < 0) {
+            this.resistencia = 0;
+        }
+    }
 
     @Override
     public int atacar(int forsa, String tipus) {
-        int atacFisic = forsa * this.getNivell();
+        int atacFisic = forsa * getNivell();
         System.out.println(getNom() + " a fet " + atacFisic + " de dany.");
         return atacFisic;
+    }
+
+    @Override
+    public void rebreDany (int quantitat) {
+        float descompte = this.resistencia / 10;
+        int danyEstalviat = (int) (quantitat * descompte);
+
+        int vidaNova = getPuntsVida() - (quantitat - danyEstalviat);
+        setPuntsVida(vidaNova);
     }
 }
