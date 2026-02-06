9a1568fea397dc29dd67a6ec9cb55ef492d6222a
Marti Quera <marti.quera@gracia.lasalle.cat>
2026-02-06 10:26:57 +0100

    mag: constructor i getter/setter de mana

--- src/Mag.java
+++ src/Mag.java
@@ -1,4 +1,13 @@
 class Mag extends Entitat {
     private int mana;
 
+    public Mag(String n, int lvl, int pv, int mana) {
+        super(n, lvl, pv);
+        setMana(mana);
+    }
+
+    // MANA
+    public int getMana() { return mana; }
+
+    public void setMana(int m) { this.mana = m; }
 }
